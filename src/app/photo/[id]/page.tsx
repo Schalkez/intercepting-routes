88c9d19b7bb35391f8291489680253e4100f233e
hiennguyen-corize <115438418+hiennguyen-corize@users.noreply.github.com>
2025-02-23 00:33:30 +0700

    Update

--- src/app/photo/[id]/page.tsx
+++ src/app/photo/[id]/page.tsx
@@ -4,6 +4,7 @@ import { photos } from "../../data";
 
 export default function PhotoPage({ params: { id } }: any) {
   const photo = photos.find((p) => p.id === id);
+
   return (
     <Image
       src={photo?.src ?? ""}
